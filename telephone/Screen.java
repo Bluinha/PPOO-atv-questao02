fa745e277116ef27a2fd7d42e5cde4179540e4a6
bluinha <cecyliahelenna@gmail.com>
2025-10-06 22:16:59 -0300

    observer que imprime o número que foi discado adicionado na UI Screen.

--- telephone/Screen.java
+++ telephone/Screen.java
@@ -6,13 +6,25 @@ import java.util.stream.Collectors;
  * Printing to the screen:
  *  System.out.println("hello");
  */
+
+
 public class Screen implements Observer {
 
     @Override
     public void update(PhoneModel model) {
         int LastDigit = model.getLastDigit();
+        List<Integer> allDigits = model.getDigits();
 
-        System.out.println("Pressionando: "+ LastDigit);
+        String fullNumber = allDigits.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        if (fullNumber.length() < 12) {
+            System.out.println("Pressionando: "+ LastDigit);
+            System.out.println(LastDigit);
+        }else if (fullNumber.length() == 12) {
+            System.out.println("Agora discando: " + fullNumber);
+        }
     }
 
 }
